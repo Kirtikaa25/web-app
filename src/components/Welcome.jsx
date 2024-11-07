a57b07cb86651e8f8a0d28f26c5a2baa14e7a3f6
Kirtika Gautam <kirtika25@Kirtikas-MacBook-Air.local>
2024-11-07 12:53:48 +0530

    initial commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome({ currentUser }) {
+
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    if (currentUser && currentUser.username) {
+      setUsername(currentUser.username);
+    }
+  }, [currentUser]);
+
+  return (
+    <Container>
+      <img src={Robot} alt="Robot" />
+      {username && <h1>Welcome, <span>{username}!</span></h1>}
+      <h3>Please select a chat to start messaging</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
